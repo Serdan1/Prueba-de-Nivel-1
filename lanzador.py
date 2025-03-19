87cd8926ca523005aa9b4818f5eff21b01bf9027
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-03-19 12:28:54 +0000

    funcionando

--- lanzador.py
+++ lanzador.py
@@ -3,26 +3,16 @@ from subclases.bicicleta import Bicicleta
 from subsubclases.camioneta import Camioneta
 from subsubclases.motocicleta import Motocicleta
 
-
 def lanzador_main():
-    vehiculos = []
-
-    vehiculos.append(Coche("rojo", 4, 150, 1200))
-    vehiculos.append(Coche("azul", 4, 200, 2000))
-    vehiculos.append(Bicicleta("verde", 2, "bh", "bmx", "urbana"))
-    vehiculos.append(Bicicleta("blanca", 2, "orbea", "r2", "deportiva"))
-    vehiculos.append(Camioneta("negro", 4, 100, 2200, "ford", "ranger", 1000))
-    vehiculos.append(Motocicleta("blanca",2,"Suzuki","GSXR", "Deportiva", 300, 999))
-    
-    
-    catalogar(vehiculos)
-    
-def catalogar(vehiculos):
-    for vehiculo in vehiculos:
-        print(f"Clase: {vehiculo.__class__.__name__}")
-        for atributo, valor in vehiculo.__dict__.items():
-            print(f"{atributo}: {valor}")
-        print()
+    vehiculos = [
+        Coche("rojo", 4, 150, 1200),
+        Coche("azul", 4, 200, 2000),
+        Bicicleta("verde", 2, "bh", "bmx", "urbana"),
+        Bicicleta("blanca", 2, "orbea", "r2", "deportiva"),
+        Camioneta("negro", 4, 100, 2200, "ford", "ranger", 1000),
+        Motocicleta("blanca", 2, "Suzuki", "GSXR", "Deportiva", 300, 999)
+    ]
+    return vehiculos  # Devolvemos la lista para usarla en main.py
 
 def catalogar(vehiculos, ruedas=None):
     if ruedas is not None:
